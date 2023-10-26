825df1c02c5d4df32cdbb73cd06baefadd8daaf9
esimpsonb <143913481+esimpsonb@users.noreply.github.com>
2023-10-26 13:01:11 -0300

    Update order.py
    Orders where not being saved

--- OOP/Classes/order.py
+++ OOP/Classes/order.py
@@ -1,6 +1,19 @@
+import os
+
+
 class Order():
 
     def __init__(self,buyer):
         self.items = buyer.cart.added_books
         self.buyer = buyer
         self.price = sum([b.price for b in self.items])
+
+    def save_changes(self):
+        directory_path = r"C:\Users\enriq\OneDrive\Desktop\Capacitacion\Python\OOP\Data\Orders_data"
+        file_count = len(os.listdir(folder_path))
+        file_path = os.path.join(directory_path, "user_"+buyer.username+str(file_count)+".pkl")
+        with open(file_path, "wb") as file:
+            pickle.dump(self, file)
+
+    def __del__(self):
+        self.save_changes()
